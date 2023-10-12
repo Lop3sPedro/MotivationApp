4515b5fd8fe7af1ae11e8d983518765bb77df712
Pedro Lopes <lopes33.pedrohenrique@gmail.com>
2023-10-12 09:52:48 -0300

    create app motivation

---
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.motivation.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.R
+import com.example.motivation.data.Mock
+import com.example.motivation.infra.SecurityPreferences
+import com.example.motivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private var categoryId = MotivationConstants.FILTER.ALL
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Esconder a barra de navegação
+        supportActionBar?.hide()
+
+        // Chamada da funções
+        handleFilter(R.id.image_all)
+        handleNextPhrase()
+
+        // Eventos
+        binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
+        binding.textUserName.setOnClickListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handleUserName()
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_new_phrase -> {
+                handleNextPhrase()
+            }
+            in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny) -> {
+                handleFilter(view.id)
+            }
+            R.id.text_user_name -> {
+                startActivity(Intent(this, UserActivity::class.java))
+            }
+        }
+    }
+
+    private fun handleNextPhrase() {
+        binding.textPhrase.text = Mock().getPhrase(categoryId)
+    }
+
+    private fun handleFilter(id: Int) {
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
+        }
+    }
+
+    private fun handleUserName() {
+        val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
+        binding.textUserName.text = "Olá, $name!"
+    }
+}
---
+++ app/src/main/java/com/example/motivation/ui/UserActivity.kt
@@ -0,0 +1,45 @@
+package com.example.motivation.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivation.R
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.infra.SecurityPreferences
+import com.example.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+
+        if (name != "") {
+            SecurityPreferences(this).stroreString(MotivationConstants.KEY.USER_NAME, name)
+
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
